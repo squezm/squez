5449c22b16b6f73ab9670a3d12a195a7497c724b
Mark Velasquez <markvelasquez@Marks-MacBook-Pro.local>
2017-12-31 12:02:07 -0500

    integrated app

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port);
 
 module.exports = app;
